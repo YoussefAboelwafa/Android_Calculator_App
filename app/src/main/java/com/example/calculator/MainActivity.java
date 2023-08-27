b5d90f927dbc606b4d12740a897be0ad58c7ae5b
Youssef Aboelwafa <96186143+YoussefAboelwafa@users.noreply.github.com>
2023-08-28 00:45:55 +0300

    Backend Done

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,12 +3,168 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public String str = "";
+    Character op = 'q';
+    int i;
+    float num,numtemp;
+    TextView showResult;
+    Button one,two,three,four,five,six,seven,eight,nine,zero,add,sub,mul,div,clear,equal;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main);
+        one = (Button)findViewById(R.id.one);
+        two = (Button)findViewById(R.id.two);
+        three = (Button)findViewById(R.id.three);
+        four = (Button)findViewById(R.id.four);
+        five = (Button)findViewById(R.id.five);
+        six = (Button)findViewById(R.id.six);
+        seven = (Button)findViewById(R.id.seven);
+        eight = (Button)findViewById(R.id.eight);
+        nine = (Button)findViewById(R.id.nine);
+        zero = (Button)findViewById(R.id.zero);
+        add = (Button)findViewById(R.id.add);
+        sub = (Button)findViewById(R.id.sub);
+        mul = (Button)findViewById(R.id.mul);
+        div = (Button)findViewById(R.id.div);
+        clear = (Button)findViewById(R.id.clear);
+        equal = (Button)findViewById(R.id.equal);
+        showResult = (TextView)findViewById(R.id.result);
+
+        one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insert(1);
+            }
+        });
+        two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insert(2);
+            }
+        });
+        three.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insert(3);
+            }
+        });
+        four.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insert(4);
+            }
+        });
+        five.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insert(5);
+            }
+        });
+        six.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insert(6);
+            }
+        });
+        seven.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insert(7);
+            }
+        });
+        eight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insert(8);
+            }
+        });
+        nine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insert(9);
+            }
+        });
+        zero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insert(0);
+            }
+        });
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                perform();
+                op = '+';
+            }
+        });
+        sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                perform();
+                op = '-';
+            }
+        });
+        mul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                perform();
+                op = '*';
+            }
+        });
+        div.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                perform();
+                op = '/';
+            }
+        });
+        equal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculate();
+            }
+        });
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                reset();
+            }
+        });
     }
+    private void insert(int i) {
+        str = str + Integer.toString(i);
+        num = Integer.valueOf(str).intValue();
+        showResult.setText(str);
+    }
+    private void reset() {
+        str = "";
+        op = 'q';
+        num = 0;
+        numtemp = 0;
+        showResult.setText("");
+    }
+    private void calculate() {
+        if(op == '+')
+            num = numtemp+num;
+        else if(op == '-')
+            num = numtemp-num;
+        else if(op == '/')
+            num = numtemp / num;
+        else if(op == '*')
+            num = numtemp*num;
+        showResult.setText(""+num);
+    }
+    private void perform() {
+        str = "";
+        numtemp = num;
+    }
+
 }
